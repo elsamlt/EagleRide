152a0ef4d22c5c5990c8e81d1d902871133bc69f
kyleighpea123 <kyleighp82@gmail.com>
2026-04-08 19:12:10 -0400

    Create UserForm.vue

---
+++ frontend/src/components/UserForm.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { computed  } from 'vue';
+
+//determine if user is logged in
+const isLoggedIn = defineProps({
+  isLoggedIn: {
+    type: Boolean,
+    default: false
+  }
+});
+
+//header logic
+const
